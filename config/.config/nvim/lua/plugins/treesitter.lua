0c625fddc1563e25da151ece6e4f3f89644a43e6
coffemugtester <coffeemugtester@gmail.com>
2025-07-19 12:10:36 +0200

    lua linter added

--- config/.config/nvim/lua/plugins/treesitter.lua
+++ config/.config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,15 @@
-return { 
-  "nvim-treesitter/nvim-treesitter", 
-  branch = 'master', 
-  lazy = false, 
-  build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = {"lua", "javascript"},
-      highlight = { enable = true},
-      indent = { enable = true},
-    })
-  end
+return {
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			--      ensure_installed = {"lua", "javascript"},
+			auto_install = true, -- makes it unnecessary to explicitly list every language for highlighting
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
