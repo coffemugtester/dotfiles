b4646f5e8696d6abe6ab7e3b0809c0ff3d37fd70
coffemugtester <coffeemugtester@gmail.com>
2025-07-19 14:32:48 +0200

    add eslint and alpha dashboard

--- config/.config/nvim/lua/plugins/none-ls.lua
+++ config/.config/nvim/lua/plugins/none-ls.lua
@@ -1,28 +1,32 @@
 return {
-	"nvimtools/none-ls.nvim", -- use :Mason to /search and (I)nstall e.g.: stylua
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.eslint_d, -- ESLint for diagnostics
-				null_ls.builtins.formatting.prettier.with({
-					-- Optionally specify filetypes:
-					filetypes = {
-						"javascript",
-						"typescript",
-						"html",
-						"css",
-						"json",
-						"yaml",
-						"markdown",
-						"vue",
-						"svelte",
-					},
-				}),
-				--        null_ls.builtins.formatting.rubocop -- remember to add diagnostics for used languages
-			},
-		})
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+  "nvimtools/none-ls.nvim",-- use :Mason to /search and (I)nstall e.g.: stylua
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier.with({
+          -- Optionally specify filetypes:
+          filetypes = {
+            "javascript",
+            "typescript",
+            "html",
+            "css",
+            "json",
+            "yaml",
+            "markdown",
+            "vue",
+            "svelte",
+          },
+        }),
+        -- require("none-ls.diagnostics.eslint_d"),
+        require("none-ls.diagnostics.eslint"),
+        -- --        null_ls.builtins.formatting.rubocop -- remember to add diagnostics for used languages
+      },
+    })
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
