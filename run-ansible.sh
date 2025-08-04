63578b03d115c4ac98c120f35a600aa38cc19c73
coffemugtester <coffeemugtester@gmail.com>
2025-08-04 13:15:21 +0200

    update ansible script

--- run-ansible.sh
+++ run-ansible.sh
@@ -6,19 +6,24 @@
 set -e
 
 # Optional: Update system packages
+echo "Update system packages"
 sudo apt update && sudo apt upgrade -y
 
 # Install software-properties-common (if missing)
+echo "Install software-properties-common (if missing)"
 sudo apt install -y software-properties-common
 
 # Add Ansible PPA
-sudo add-apt-repository --yes --update ppa:ansible/ansible
+# sudo add-apt-repository --yes --update ppa:ansible/ansible
 
 # Install Ansible
+echo "Install Ansible"
 sudo apt install -y ansible
 
 # Verify installation
+echo "Verify installation"
 ansible --version
 
 # Pull play
+echo "Pull playbook"
 ansible-pull -U https://github.com/coffemugtester/dotfiles playbook.yml
