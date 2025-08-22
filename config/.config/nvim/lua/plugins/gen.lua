331961a8e51b624398135f4fbf1891c5c7420484
daniel.alfaro <daniel.alfaro@adevinta.com>
2025-08-22 23:03:22 +0200

    disable ollama thinking

--- config/.config/nvim/lua/plugins/gen.lua
+++ config/.config/nvim/lua/plugins/gen.lua
@@ -3,6 +3,16 @@ return {
 	config = function()
 		require("gen").setup({
 			model = "qwen3:14b", -- The default model to use.
+			show_model = true,
+			think = false, -- Whether to show thinking messages.
+			body = { think = false, stream = true },
+			command = function(options)
+				return "curl --silent --no-buffer -X POST http://"
+					.. options.host
+					.. ":"
+					.. options.port
+					.. "/api/chat -d $body"
+			end,
 		})
 		vim.keymap.set({ "n", "v" }, "<leader>o", ":Gen<CR>")
 	end,
