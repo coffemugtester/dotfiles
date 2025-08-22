58759ad3a53facd6a2f76290135d77866e62b79d
coffemugtester <coffeemugtester@gmail.com>
2025-08-22 20:24:27 +0200

    update config

--- config/.config/nvim/lua/plugins/gen.lua
+++ config/.config/nvim/lua/plugins/gen.lua
@@ -2,7 +2,7 @@ return {
 	"David-Kunz/gen.nvim",
 	config = function()
 		require("gen").setup({
-			model = "gpt-oss:20b", -- The default model to use.
+			model = "qwen3:14b", -- The default model to use.
 		})
 		vim.keymap.set({ "n", "v" }, "<leader>o", ":Gen<CR>")
 	end,
