8a0ec787bf014f85d6a0b7e2e17bfb5fd8a097fd
coffemugtester <coffeemugtester@gmail.com>
2025-11-21 22:48:47 +0100

    new layout

---
+++ tmux/.tmux-layouts/claud-editor.window.sh
@@ -0,0 +1,27 @@
+# Set window root path. Default is `$session_root`.
+# Must be called before `new_window`.
+# window_root "${PWD}"
+
+# Create new window. If no argument is given, window name will be based on
+# layout file name.
+window_root "${PWD}"
+new_window "claude-editor"
+
+run_cmd "nvim"     # runs in active pane
+
+# Split window into panes.
+split_h 10
+send_keys "air"
+
+split_v 30
+run_cmd "gst"
+
+# Run commands.
+#run_cmd "date" 1  # runs in pane 1
+
+# Paste text
+#send_keys "top"    # paste into active pane
+#send_keys "date" 1 # paste into pane 1
+
+# Set active pane.
+select_pane 1
